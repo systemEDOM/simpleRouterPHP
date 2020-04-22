615467f431a7d9c597a26c2c923e1dca1f631588
Daniel Oseguera <codigito.c@gmail.com>
2020-04-22 01:18:42 -0500

    uncommented samples

--- samples/index.php
+++ samples/index.php
@@ -13,15 +13,15 @@ $router->post('/', function () {
     echo "post";
 });
 $router->put('/', function () {
-    echo "put" . $_POST['name'];
+    echo "put";
 });
 $router->delete('/', function () {
-    echo "delete" . $_REQUEST['name'];
+    echo "delete";
 });
-/*$router->get('/users/[i:id]/hi/[i:slug]', function () {
+$router->get('/users/[i:id]/hi/[i:slug]', function () {
     echo "params";
 });
-$router->get('/hi/[i:id]/[i:slug]', "HomeController@showHome");*/
+$router->get('/hi/[i:id]/[i:slug]', "HomeController@showHome");
 
 
 $router->match();
